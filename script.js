d9dc31a194fca1a29e414f454846ef965868f919
phillipdann-ward <160318179+phillipdann-ward@users.noreply.github.com>
2026-08-12 15:15:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // The special item
     const specialItemImage = 'images/areatsrs.png';
-    const specialItemProbability = 0.1; // 30% chance for the special item to appear
+    const specialItemProbability = 1.0; // 30% chance for the special item to appear
     const specialItemSizeMultiplier = 0.5; // Make the special item a bit larger
 
     const numberOfRegularItemsToThrow = 14; // We'll throw one less regular item if the special one appears
